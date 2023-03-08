1ff86ce2770fd198a09fbb2b08bfe4e6cb675ef2
kendalldrake <112967655+kendalldrake@users.noreply.github.com>
2023-03-07 21:13:31 -0500

    Update main.js
    Added Javascript

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,31 @@
 window.arenaCallback = () => {
 	// Put your JavaScript in here!
 	// This will run after Are.na’s API returns your data.
+
+//GET ALL OF MY IMAGE BLOCKS; QUERY SELECTOR ALL TAKES A CSS SELECTOR
+let imageBlocks = document.querySelectorAll('li.image-block')
+console.log(imageBlocks)
+
+// GO THROUGH ALL OF MY IMAGE BLOCKS
+imageBlocks.forEach((block)=> {
+	// console.log(block)
+
+// MAKE THE BLOCKS REACT ON CLICK
+
+// TARGET THE FIGURE ELEMENT OF THE BLOCK
+	block.querySelector('figure').onclick = () =>{
+
+// ADD A CLASS CALLED ACTIVE
+	block.classList.add('active')	
+	}
+
+// TARGET THE ASIDE ELEMENT OF THE BLOCK
+block.querySelector('aside').onclick = () => {
+
+// REMOVE A CLASS CALLED ACTIVE
+     block.classList.remove('active')
+
 }
+})
+}
+
